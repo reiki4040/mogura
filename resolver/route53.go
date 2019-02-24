8b994ad4355dade93df552773b209c40ede70d73
reiki4040 <reiki4040@gmail.com>
2019-02-25 00:03:42 +0900

    removed sample code.

--- resolver/route53.go
+++ resolver/route53.go
@@ -2,7 +2,6 @@ package resolver
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -47,7 +46,6 @@ func (r *Route53Resolver) Load() error {
 			continue
 		}
 
-		log.Printf("hosted zone name: %s (%s)", aws.StringValue(z.Name), aws.StringValue(z.Config.Comment))
 		in := &route53.ListResourceRecordSetsInput{
 			HostedZoneId: z.Id,
 		}
@@ -109,32 +107,4 @@ func (r *Route53Resolver) Resolve(zoneId, name string) (Route53Result, error) {
 	}
 
 	return result, nil
-}
-
-func Sample() {
-	region := "ap-northeast-1"
-
-	sess := session.Must(session.NewSession(&aws.Config{
-		Region: aws.String(region),
-	}))
-
-	svc := route53.New(sess)
-	resp, err := svc.ListHostedZones(&route53.ListHostedZonesInput{})
-	if err != nil {
-		log.Fatalf("failed list hosted zones: %v", err)
-	}
-	for _, z := range resp.HostedZones {
-		log.Printf("hosted zone name: %s (%s)", aws.StringValue(z.Name), aws.StringValue(z.Config.Comment))
-		in := &route53.ListResourceRecordSetsInput{
-			HostedZoneId: z.Id,
-		}
-		r, err := svc.ListResourceRecordSets(in)
-		if err != nil {
-			log.Printf("failed list hosted zones: %v", err)
-			continue
-		}
-		for _, rs := range r.ResourceRecordSets {
-			log.Printf("record set %s %s TTL %d", aws.StringValue(rs.Type), aws.StringValue(rs.Name), aws.Int64Value(rs.TTL))
-		}
-	}
 }
