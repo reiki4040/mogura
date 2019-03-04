5d3ba53ef05e63a5ab80299bd32fab8a1630e602
reiki4040 <reiki4040@gmail.com>
2019-03-04 12:51:21 +0900

    removed route53 feature
    remote DNS is auto resolved in ssh client connection.
    and SRV resolved with DNS request via ssh.

--- mogura/target.go
+++ mogura/target.go
@@ -2,14 +2,8 @@ package mogura
 
 import (
 	"fmt"
-	"github.com/reiki4040/mogura/resolver"
 	"golang.org/x/crypto/ssh"
 	"strconv"
-	"strings"
-)
-
-var (
-	route53Resolver = resolver.NewRoute53Resolver("ap-northeast-1")
 )
 
 type Target struct {
@@ -32,33 +26,6 @@ func (t Target) Resolve(conn *ssh.Client, resolver string) (string, error) {
 
 		// TODO if priority are same, then shuffle
 		return srvs[0].TargetPort(), nil
-	case "ROUTE53":
-		// TODO resolve private hosted zone via Route53 API (not DNS request)
-		splited := strings.Split(t.Target, ":")
-		if len(splited) != 2 {
-			return "", fmt.Errorf("invalid Route53 type remote_name: %s. format is hostedzoneId:DNSName", t.Target)
-		}
-
-		result, err := route53Resolver.Resolve(splited[0], splited[1])
-		if err != nil {
-			return "", err
-		}
-
-		switch result.Type {
-		case "A":
-			// TODO random get values(records)
-			detectedRemote := result.Values[0] + ":" + strconv.Itoa(t.TargetPort)
-			return detectedRemote, nil
-		case "CNAME":
-			// TODO CNAME
-			return "", fmt.Errorf("not yet implemented CNAME.")
-		case "SRV":
-			// TODO SRV
-			return "", fmt.Errorf("not yet implemented SRV.")
-		default:
-			return "", fmt.Errorf("unknown Route53 record type: %s", result.Type)
-		}
-
 	case "HOST-PORT":
 		fallthrough
 	default:
